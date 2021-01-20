e5eb1b73b488f883b9dbd19a3fa688a851e29607
vlad-grom <56732700+vlad-grom@users.noreply.github.com>
2021-01-20 15:36:37 +0700

    New

--- Chandra_finish/skin.js
+++ Chandra_finish/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.1/17227
 // Filename: silhouette_v6.ggsk
-// Generated вт янв. 19 13:54:37 2021
+// Generated ср янв. 20 15:33:05 2021
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
